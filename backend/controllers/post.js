7e157d61b2a84af997442453b16716c7faeca45d
Robin <r.sgd@laposte.net>
2022-02-19 16:16:39 +0100

    commit 20220219 16h16 backend en cours

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,27 +1,28 @@
-const Sauce = require('../models/Thing'); // à adapter pour chaque model rdx
+//const Post = require('../models/Thing'); // à adapter pour chaque model rdx
+const Post = require('../models/Post'); // à adapter pour chaque model rdx
 const fs = require('fs');
 
-//console.log("ok passé ici sauce.js ligne 3");// test en dev
+//console.log("ok passé ici post.js ligne 3");// test en dev
 
-exports.createSauce  = (req, res, next) => { 
-  const sauceObject = JSON.parse(req.body.sauce);
+exports.createPost  = (req, res, next) => { 
+  const postObject = JSON.parse(req.body.post);
   //delete req.body._id;
-  delete sauceObject._id;
-  const sauce = new Sauce({
+  delete postObject._id;
+  const post = new Post({
     //...req.body,
-	...sauceObject,
+	...postObject,
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
-  sauce.save()
+  post.save()
     .then(() => res.status(201).json({message: 'Objet enregistré !'})) 
     .catch(error => res.status(400).json({ error }));  
 };
   
-exports.modifySauce = (req, res, next) => {
+exports.modifyPost = (req, res, next) => {
   // cas particulier SI NOUVELLE IMAGE alors enlever la précédente - - - - -
   if (req.file) {
-    Sauce.findOne({ _id: req.params.id }).then((sauce) => {
-      const filename = sauce.imageUrl.split('/images/')[1];
+    Post.findOne({ _id: req.params.id }).then((post) => {
+      const filename = post.imageUrl.split('/images/')[1];
       fs.unlink(`images/${filename}`, (err) => {
         if (err) console.log(err);
         else {
@@ -31,24 +32,24 @@ exports.modifySauce = (req, res, next) => {
     });
   }
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    const sauceObject = req.file ?
+    const postObject = req.file ?
     {
-      ...JSON.parse(req.body.sauce),
+      ...JSON.parse(req.body.post),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };  
 
-  // Sauce.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
-  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+  // Post.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+  Post.updateOne({ _id: req.params.id }, { ...postObject, _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Objet modifié !'}))
     .catch(error => res.status(400).json({ error }));
 };
 
-exports.deleteSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-  .then(sauce => {
-    const filename = sauce.imageUrl.split('/images/')[1];
+exports.deletePost = (req, res, next) => {
+  Post.findOne({ _id: req.params.id })
+  .then(post => {
+    const filename = post.imageUrl.split('/images/')[1];
     fs.unlink(`images/${filename}`, () => {
-      Sauce.deleteOne({ _id: req.params.id })
+      Post.deleteOne({ _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
       .catch(error => res.status(400).json({ error }));
       });
@@ -58,18 +59,18 @@ exports.deleteSauce = (req, res, next) => {
 
 
 
-exports.getOneSauce = (req, res, next) => {
-      Sauce.findOne({ _id: req.params.id })
-        .then((sauce) => { res.status(200).json(sauce);})
+exports.getOnePost = (req, res, next) => {
+      Post.findOne({ _id: req.params.id })
+        .then((post) => { res.status(200).json(post);})
         .catch((error) => { res.status(404).json({ error: error });});
 };
 
   
 
-exports.getAllSauces = (req, res, next) => {
-  Sauce.find()
-    .then((sauces) => {
-      res.status(200).json(sauces);
+exports.getAllPosts = (req, res, next) => {
+  Post.find()
+    .then((posts) => {
+      res.status(200).json(posts);
     })
     .catch((error) => {
       res.status(400).json({
@@ -85,7 +86,7 @@ exports.likeDislike = (req, res, next) => {
 
     // case 1 pour like
     case 1:
-      Sauce.updateOne(
+      Post.updateOne(
         { _id: req.params.id },
         { $push: { usersLiked: req.body.userId }, // $push ajoute une valeur spécifiée à un tableau
           $inc: { likes: +1 } // $inc incrémente un champ d'une valeur spécifiée 
@@ -96,7 +97,7 @@ exports.likeDislike = (req, res, next) => {
 
     // case -1 pour dislike
     case -1:
-      Sauce.updateOne(
+      Post.updateOne(
         { _id: req.params.id },
         { $push: { usersDisliked: req.body.userId },  // $push ajoute une valeur spécifiée à un tableau
           $inc: { dislikes: +1 } // $inc incrémente un champ d'une valeur spécifiée 
@@ -109,10 +110,10 @@ exports.likeDislike = (req, res, next) => {
 
     // case 0 décrémentation du like ou du dislike
     case 0:
-      Sauce.findOne({ _id: req.params.id })
-        .then((sauce) => {
-          if (sauce.usersLiked.includes(req.body.userId)) { // ici l'utilisateur avait aimé 
-            Sauce.updateOne(
+      Post.findOne({ _id: req.params.id })
+        .then((post) => {
+          if (post.usersLiked.includes(req.body.userId)) { // ici l'utilisateur avait aimé 
+            Post.updateOne(
               { _id: req.params.id },
               { $pull: { usersLiked: req.body.userId }, // $push ajoute une valeur spécifiée à un tableau
                 $inc: { likes: -1 } // $inc incrémente un champ d'une valeur spécifiée
@@ -122,8 +123,8 @@ exports.likeDislike = (req, res, next) => {
               )
               .catch((error) => res.status(400).json({ error }));
           }
-          if (sauce.usersDisliked.includes(req.body.userId)) {  // ici l'utilisateur n'avait pas aimé 
-            Sauce.updateOne(
+          if (post.usersDisliked.includes(req.body.userId)) {  // ici l'utilisateur n'avait pas aimé 
+            Post.updateOne(
               { _id: req.params.id },
               {
                 $pull: { usersDisliked: req.body.userId },  // $push ajoute une valeur spécifiée à un tableau
